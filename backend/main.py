f2a6834165ab7f961b6d6cf5ac115da05bdde374
Raquel Diaz <raquelmdiaz@hotmail.com>
2026-05-20 15:19:49 +0200

    updated allowed origins

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5500",   # local dev
+        "https://ai-learning-journal-lake.vercel.app",  # production
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
